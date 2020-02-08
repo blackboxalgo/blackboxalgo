82f8433800e6143669ab7e62852eb45991081e5c
blackboxalgo <59296384+blackboxalgo@users.noreply.github.com>
2020-02-08 12:30:10 -0500

    New

--- Python Code/ClassificationSignalCreater.py
+++ Python Code/ClassificationSignalCreater.py
@@ -3,12 +3,17 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 import numpy as np
 
-datamatrix = pd.read_csv(r'G:\Python Data\FactSet Alpha Testing Export\Data Export - 55.csv')
+datamatrix = pd.read_csv(r'G:\Python Data\FactSet Alpha Testing Export\datarobotextract.txt')
+
+#datamatrix2 = datamatrix
+#datamatrix = datamatrix2[datamatrix2['Div Yld'] > 0.028]
+
+number_of_companies = datamatrix.groupby(['Period (YYYYMMDD)']).size()
 
 unique_dates = datamatrix['Period (YYYYMMDD)']
-datamatrix.columns
+all_features = datamatrix.columns
 
-#top third
+#quantile returns
 quintile_period_returns_2 = datamatrix.groupby('Period (YYYYMMDD)')['Universe Returns Additional Return 2'].quantile(1/2)
 quintile_period_returns_3 = datamatrix.groupby('Period (YYYYMMDD)')['Universe Returns Additional Return 3'].quantile(1/2)
 quintile_period_returns_4 = datamatrix.groupby('Period (YYYYMMDD)')['Universe Returns Additional Return 4'].quantile(1/2)
@@ -33,5 +38,7 @@ merged_table.loc[merged_table['Universe Returns Additional Return 3_x'] > merged
 merged_table.loc[merged_table['Universe Returns Additional Return 4_x'] > merged_table['Universe Returns Additional Return 4_y'], 'Outperform 6-Month'] = 1
 merged_table.loc[merged_table['Universe Returns Additional Return 5_x'] > merged_table['Universe Returns Additional Return 5_y'], 'Outperform 12-Month'] = 1
 final_table = merged_table.fillna('NA')
-final_table.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\Data Export - 55 Top Half.csv',index=False)
+number_of_companies.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\Number of Companies - All 100M.csv',index=True)
+all_features.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\All Features.csv',index=True)
+final_table.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\Data Export All 100M.csv',index=False)
 
--- Python Code/DataRobot - Connection.py
+++ Python Code/DataRobot - Connection.py
@@ -7,13 +7,17 @@ print(dr.__version__)
 with open (r"G:\DataRobot\JNDataRobotAPIKey.txt", "r") as myfile:
     VPNToken=myfile.readlines()
 
-
 #Fill in the API token.You can get the
 API_TOKEN = VPNToken
 END_POINT = 'https://datarobot.factset.com/api/v2'
 
 #Define few global variables here
 TRAINING_FILE_PATH = r'G:\Python Data\FactSet Alpha Testing Extracts\Data Export.txt'
+data_raw_file = pd.read_csv(r'G:\Python Data\FactSet Alpha Testing Export\Data Export - 3 Yield All.csv')
+project_name = '1832 Retirement Yield Over 3 All'
+
+all_columns = pd.DataFrame(data_raw_file.columns)
+all_columns.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\All Columns.csv',index=False)
 
 #Instantiate the client 
 dr.Client(token= API_TOKEN, endpoint=END_POINT)
@@ -21,3 +25,16 @@ dr.Client(token= API_TOKEN, endpoint=END_POINT)
 #See theat you can navigate the current projects
 current_projects  = dr.Project.list()
 current_projects
+
+#start a new project
+target_name = 'Outperform 12-Month'
+project = dr.Project.create(data_raw_file, project_name=project_name)
+project.set_target(target=target_name,
+                   metric='LogLoss',
+                   mode=dr.AUTOPILOT_MODE.FULL_AUTO)
+
+project.get_status()
+project.get_modeling_features()
+
+used_features = project.get_features()
+used_features
