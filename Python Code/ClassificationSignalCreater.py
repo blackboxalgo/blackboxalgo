8408123ee1bca20b917547434aa90b895c63f064
blackboxalgo <59296384+blackboxalgo@users.noreply.github.com>
2020-02-11 00:19:46 -0500

    Update ClassificationSignalCreater.py
    test1

--- Python Code/ClassificationSignalCreater.py
+++ Python Code/ClassificationSignalCreater.py
@@ -5,13 +5,15 @@ import numpy as np
 
 datamatrix = pd.read_csv(r'G:\Python Data\FactSet Alpha Testing Export\datarobotextract.txt')
 
-#datamatrix2 = datamatrix
-#datamatrix = datamatrix2[datamatrix2['Div Yld'] > 0.028]
+datamatrix['MktVal Co']
+datamatrix = datamatrix[datamatrix['MktVal Co'] > 10000]
+datamatrix
 
 number_of_companies = datamatrix.groupby(['Period (YYYYMMDD)']).size()
 
 unique_dates = datamatrix['Period (YYYYMMDD)']
 all_features = datamatrix.columns
+
 
 #quantile returns
 quintile_period_returns_2 = datamatrix.groupby('Period (YYYYMMDD)')['Universe Returns Additional Return 2'].quantile(1/2)
@@ -38,7 +40,7 @@ merged_table.loc[merged_table['Universe Returns Additional Return 3_x'] > merged
 merged_table.loc[merged_table['Universe Returns Additional Return 4_x'] > merged_table['Universe Returns Additional Return 4_y'], 'Outperform 6-Month'] = 1
 merged_table.loc[merged_table['Universe Returns Additional Return 5_x'] > merged_table['Universe Returns Additional Return 5_y'], 'Outperform 12-Month'] = 1
 final_table = merged_table.fillna('NA')
-number_of_companies.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\Number of Companies - All 100M.csv',index=True)
+number_of_companies.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\Number of Companies - All 10B.csv',index=True)
 all_features.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\All Features.csv',index=True)
-final_table.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\Data Export All 100M.csv',index=False)
+final_table.to_csv(r'G:\Python Data\FactSet Alpha Testing Export\Data Export All 10B.csv',index=False)
 
