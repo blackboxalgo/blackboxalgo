c26b13a44ffe921879688917a92e01758bb4d467
blackboxalgo <59296384+blackboxalgo@users.noreply.github.com>
2020-03-04 19:15:49 -0500

    f

--- Python Code/ClassificationSignalCreater.py
+++ Python Code/ClassificationSignalCreater.py
@@ -3,7 +3,7 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 import numpy as np
 
-file_name = 'Data Export - 10.csv'
+file_name = 'dr_extract_dram.txt'
 
 datamatrix = pd.read_csv(fr'G:\Python Data\FactSet Alpha Testing Export\{file_name}')
 
@@ -14,7 +14,6 @@ number_of_companies = datamatrix.groupby(['Period (YYYYMMDD)']).size()
 
 unique_dates = datamatrix['Period (YYYYMMDD)']
 all_features = datamatrix.columns
-
 
 #quantile returns
 quintile_period_returns_2 = datamatrix.groupby('Period (YYYYMMDD)')['Universe Returns Additional Return 2'].quantile(1/2)
@@ -44,4 +43,3 @@ final_table = merged_table.fillna('NA')
 number_of_companies.to_csv(fr'G:\Python Data\FactSet Alpha Testing Export\Number of Companies - {file_name}.csv',index=True)
 #all_features.to_csv(fr'G:\Python Data\FactSet Alpha Testing Export\Features - {file_name}.csv',index=True)
 final_table.to_csv(fr'G:\Python Data\FactSet Alpha Testing Export\DR - {file_name}',index=False)
-
